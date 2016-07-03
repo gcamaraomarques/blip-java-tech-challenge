c45c1b2e525179e5f1b7bb9f17f4bc12f98f8e5e
Guilherme Camarao Marques <gmarques.ox@gmail.com>
2016-07-02 23:57:49 -0300

    Ajust in method.

--- src/main/java/pt/blip/gcm/purchase/repository/PurchaseRepository.java
+++ src/main/java/pt/blip/gcm/purchase/repository/PurchaseRepository.java
@@ -2,12 +2,14 @@ package pt.blip.gcm.purchase.repository;
 
 import pt.blip.gcm.purchase.domain.Purchase;
 
+import java.time.OffsetDateTime;
+
 /**
  * Created by gcamaraomarques on 2/7/16.
  */
 public interface PurchaseRepository {
     Purchase save(Purchase purchase);
-    Iterable<Purchase> findAll();
+    Iterable<Purchase> findAllValidUntil(OffsetDateTime expireDate);
     Purchase findOne(Long id);
     void delete(Long id);
 }
