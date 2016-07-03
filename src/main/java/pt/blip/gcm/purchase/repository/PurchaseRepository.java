dadfb31e986450ce199f6ccf87f8745719c9bcc3
Guilherme Camarao Marques <gmarques.ox@gmail.com>
2016-07-03 04:28:01 -0300

    Using those exceptions.

--- src/main/java/pt/blip/gcm/purchase/repository/PurchaseRepository.java
+++ src/main/java/pt/blip/gcm/purchase/repository/PurchaseRepository.java
@@ -1,6 +1,8 @@
 package pt.blip.gcm.purchase.repository;
 
 import pt.blip.gcm.purchase.domain.Purchase;
+import pt.blip.gcm.purchase.exception.PurchaseException;
+import pt.blip.gcm.purchase.exception.PurchaseNotFoundException;
 
 import java.time.OffsetDateTime;
 
@@ -8,8 +10,8 @@ import java.time.OffsetDateTime;
  * Created by gcamaraomarques on 2/7/16.
  */
 public interface PurchaseRepository {
-    Purchase save(Purchase purchase);
-    Iterable<Purchase> findAllValidUntil(OffsetDateTime expireDate);
-    Purchase findOne(Long id);
-    void delete(Long id);
+    Purchase save(Purchase purchase) throws PurchaseException;
+    Iterable<Purchase> findAllValidUntil(OffsetDateTime expireDate) throws PurchaseException;
+    Purchase findOne(Long id) throws PurchaseNotFoundException, PurchaseException;
+    void delete(Long id) throws PurchaseNotFoundException, PurchaseException;
 }
