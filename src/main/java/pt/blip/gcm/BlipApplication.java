69f1eb1972ec3afaa11a50e5faed93694b8de068
Guilherme Camarao Marques <gmarques.ox@gmail.com>
2016-07-02 22:19:22 -0300

    Initial Commit.
    Auto generated files by IDE.

---
+++ src/main/java/pt/blip/gcm/BlipApplication.java
@@ -0,0 +1,12 @@
+package pt.blip.gcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlipApplication.class, args);
+	}
+}
